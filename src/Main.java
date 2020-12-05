2b8ca660b180c2ccce7d39eedccd1ad66960e66c
francois-71 <francoisdion71@gmail.com>
2020-12-05 16:59:10 +0100

    on tessst

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main{
+
     public static void main(String[] args){
         System.out.println("hello");
+        powpow();
     }
-    public
+    public static void powpow(){
+        int a = 3;
+        int b = 4;
+        System.out.println(a + b);
+    }
 }
