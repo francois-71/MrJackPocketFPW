9498b4b846664c657311bc8c0cd4a21a480a0672
francois-71 <francoisdion71@gmail.com>
2020-12-06 21:28:21 +0100

    création alibis class

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,5 @@
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello");
-        powpow();
-        powpow();
-        System.out.println("powpowlaplusbelle");
-    }
-    public static void powpow(){
-        int a = 3;
-        int b = 4;
-        System.out.println(a + b);
+    public static void main(){
     }
 }
